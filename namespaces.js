f3f86e0aa513d1c120607c7690d6448ae5e369cf
Bert Spaan <bert.spaan@gmail.com>
2015-07-06 15:08:46 +0200

    added dbpedia, wikidata, gemeentegeschiedenis, kloeke, pleiades!

--- namespaces.js
+++ namespaces.js
@@ -8,6 +8,13 @@ module.exports = {
   geonames: {
     baseUrl: 'http://sws.geonames.org/',
 
+    urlMatch: function(url) {
+      var alternativeBaseUrl = 'http://www.geonames.org/';
+      return url.toLowerCase().indexOf(this.baseUrl) === 0 || url.toLowerCase().indexOf(alternativeBaseUrl) === 0;
+    },
+
+    baseUrlRegex: /http:\/\/(sws|www)\.geonames\.org\//,
+
     URLtoURN: function(url) {
       var match = /.*?(\d+).*/.exec(url);
       return prefix('geonames') + match[1];
@@ -17,6 +24,7 @@ module.exports = {
       return this.baseUrl + nss + '/';
     }
   },
+
   tgn: {
     baseUrl: 'http://vocab.getty.edu/tgn/',
 
@@ -29,5 +37,71 @@ module.exports = {
     URNtoURL: function(nid, nss) {
       return this.baseUrl + nss.split(':').join('/');
     }
+  },
+
+  dbpedia: {
+    baseUrl: 'http://dbpedia.org/',
+
+    URLtoURN: function(url) {
+      var match = /(resource|page)\/(.*)$/i.exec(url);
+      return prefix('dbpedia') + match[2];
+    },
+
+    URNtoURL: function(nid, nss) {
+      return this.baseUrl + 'resource/' + nss;
+    }
+  },
+
+  wikidata: {
+    baseUrl: 'http://www.wikidata.org/',
+
+    URLtoURN: function(url) {
+      var match = /(wiki|entity)\/(.*)$/i.exec(url);
+      return prefix('wikidata') + match[2];
+    },
+
+    URNtoURL: function(nid, nss) {
+      return this.baseUrl + 'entity/' + nss;
+    }
+  },
+
+  pleiades: {
+    baseUrl: 'http://pleiades.stoa.org/',
+
+    URLtoURN: function(url) {
+      var match = /places\/(.*)$/i.exec(url);
+      return prefix('pleiades') + match[1];
+    },
+
+    URNtoURL: function(nid, nss) {
+      return this.baseUrl + 'places/' + nss;
+    }
+  },
+
+  gemeentegeschiedenis: {
+    baseUrl: 'http://www.gemeentegeschiedenis.nl/',
+
+    URLtoURN: function(url) {
+      var match = /gemeentenaam\/(.*)$/i.exec(url);
+      return prefix('gemeentegeschiedenis') + match[1];
+    },
+
+    URNtoURL: function(nid, nss) {
+      return this.baseUrl + 'gemeentenaam/' + nss;
+    }
+  },
+
+  kloeke: {
+    baseUrl: 'http://www.meertens.knaw.nl/kloeke/',
+
+    URLtoURN: function(url) {
+      var match = /kloekenummer=(.*)$/i.exec(url);
+      return prefix('kloeke') + match[1];
+    },
+
+    URNtoURL: function(nid, nss) {
+      return this.baseUrl + 'index.php?kloekenummer=' + nss;
+    }
   }
+
 };
--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ describe('uri-normalizer', function() {
        // GeoNames
        assert.equal(uriNormalizer.normalize('http://sWS.geonames.org/2758064/about.rdf'), 'urn:hg:geonames:2758064');
        assert.equal(uriNormalizer.normalize('http://sws.geonames.org/2758064/'), 'urn:hg:geonames:2758064');
+       assert.equal(uriNormalizer.normalize('http://www.geonames.org/2758064/'), 'urn:hg:geonames:2758064');
        assert.equal(uriNormalizer.normalize('hTTp://sws.geonAmEs.org/2758064'), 'urn:hg:geonames:2758064');
 
        // TGN
@@ -21,7 +22,7 @@ describe('uri-normalizer', function() {
        assert.equal(uriNormalizer.normalize('7006952', 'fOo'), 'urn:hgid:foo/7006952');
        assert.equal(uriNormalizer.normalize('flEp.tozz/352.466', 'hrmz'), 'urn:hgid:flep/352.466');
      });
-	  
+
      it('Should return urn:hg:foo URNs for known namepspaces', function() {
        // HGIDs
        assert.equal(uriNormalizer.normalize('tgn/7006952', 'foo'), 'urn:hg:tgn:7006952');
@@ -29,7 +30,7 @@ describe('uri-normalizer', function() {
      });
    });
 
-	
+
   describe('URLtoURN', function() {
     it('Should return proper URNs', function() {
 
@@ -53,6 +54,23 @@ describe('uri-normalizer', function() {
       // TGN
       assert.equal(uriNormalizer.URLtoURN('http://vocab.getty.edu/tgn/7006952'), 'urn:hg:tgn:7006952');
       assert.equal(uriNormalizer.URLtoURN('http://vocab.getty.edu/tgn/term/352466'), 'urn:hg:tgn:term:352466');
+
+      // DBpedia
+      assert.equal(uriNormalizer.URLtoURN('http://dbpedia.org/resource/Bussum'), 'urn:hg:dbpedia:Bussum');
+      assert.equal(uriNormalizer.URLtoURN('http://dbpedia.org/page/Bussum'), 'urn:hg:dbpedia:Bussum');
+
+      // WikiData
+      assert.equal(uriNormalizer.URLtoURN('http://www.wikidata.org/wiki/Q47887'), 'urn:hg:wikidata:Q47887');
+      assert.equal(uriNormalizer.URLtoURN('http://www.wikidata.org/entity/Q47887'), 'urn:hg:wikidata:Q47887');
+
+      // Pleiades
+      assert.equal(uriNormalizer.URLtoURN('http://pleiades.stoa.org/places/99003/lugdunum'), 'urn:hg:pleiades:99003/lugdunum');
+      // Kloekecodes
+      assert.equal(uriNormalizer.URLtoURN('http://www.meertens.knaw.nl/kloeke/index.php?kloekenummer=A001a'), 'urn:hg:kloeke:A001a');
+
+      // Gemeentegeschiedenis
+      assert.equal(uriNormalizer.URLtoURN('http://www.gemeentegeschiedenis.nl/gemeentenaam/Adorp'), 'urn:hg:gemeentegeschiedenis:Adorp');
+
     });
   });
 
@@ -65,6 +83,21 @@ describe('uri-normalizer', function() {
       // TGN
       assert.equal(uriNormalizer.URNtoURL('urn:hg:tgn:7006952'), 'http://vocab.getty.edu/tgn/7006952');
       assert.equal(uriNormalizer.URNtoURL('urn:hg:tgn:term:352466'), 'http://vocab.getty.edu/tgn/term/352466');
+
+      // DBpedia
+      assert.equal(uriNormalizer.URNtoURL('urn:hg:dbpedia:Bussum'),'http://dbpedia.org/resource/Bussum');
+
+      // WikiData
+      assert.equal(uriNormalizer.URNtoURL('urn:hg:wikidata:Q47887'), 'http://www.wikidata.org/entity/Q47887');
+
+      // Pleiades
+      assert.equal(uriNormalizer.URNtoURL('urn:hg:pleiades:99003/lugdunum'), 'http://pleiades.stoa.org/places/99003/lugdunum');
+      // Kloekecodes
+      assert.equal(uriNormalizer.URNtoURL('urn:hg:kloeke:A001a'), 'http://www.meertens.knaw.nl/kloeke/index.php?kloekenummer=A001a');
+
+      // Gemeentegeschiedenis
+      assert.equal(uriNormalizer.URNtoURL('urn:hg:gemeentegeschiedenis:Adorp'), 'http://www.gemeentegeschiedenis.nl/gemeentenaam/Adorp');
+
     });
   });
 
